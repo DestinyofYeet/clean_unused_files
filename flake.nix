16cd680cfd99d00297861313a234c859003a42d5
DestinyofYeet <olebend@gmail.com>
2024-08-29 14:31:14 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Clean unused files from qbittorrent";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+let
+  pkgs = import nixpkgs { system = "x86_64-linux"; };
+in {
+
+    packages.x86_64-linux.clean-unused-files = pkgs.callPackage ./pkg.nix {};
+
+    nixosModules.clean-unused-files = import ./module.nix self;
+    
+  };
+}
